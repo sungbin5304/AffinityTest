dddcd84754a2a6f08d3b4b64e1d65a27c13c119d
sungbin5304 <sungbin.dev@gmail.com>
2020-08-15 16:22:56 +0900

    :dagger: android affinity test project :dagger:

---
+++ app/src/main/java/com/sungbin/affinitytest/SecondActivity.kt
@@ -0,0 +1,16 @@
+package com.sungbin.affinitytest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+/**
+ * Created by SungBin on 2020-08-15.
+ */
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
